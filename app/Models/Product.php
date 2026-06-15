86d6c9c9e79716f68f46d450337b00422c7db241
unknown <mariamaboshalby>
2026-06-15 12:57:01 +0300

    lazy loading

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -83,6 +83,11 @@ class Product extends Model implements HasMedia
     }
 
     /* ── Relations ── */
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category', 'slug');
+    }
+
     public function orderItems()
     {
         return $this->hasMany(OrderItem::class);
